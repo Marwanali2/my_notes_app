de6a65395d376e0234ec3cecc61b35c0aa937bd4
marwan ali <marwanalizz42@gmail.com>
2023-03-19 18:19:20 +0200

    add 'addNote' method to 'add_notes_cubit' file

--- lib/cubit/add_notes_cubit/add_notes_cubit.dart
+++ lib/cubit/add_notes_cubit/add_notes_cubit.dart
@@ -1,3 +1,6 @@
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:my_notes_app/constants.dart';
+
 import '../../models/note_model.dart';
 import 'add_notes_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -5,5 +8,14 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(AddNoteInitialState());
 
-  addNote(NoteModel note) {}
+  addNote(NoteModel note) async {
+    emit(AddNoteLoadingState());
+    try {
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+      await notesBox.add(note);
+      emit(AddNoteSuccessState());
+    } on Exception catch (e) {
+      AddNoteFailureState(errorMessage: e.toString());
+    }
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -5,17 +5,17 @@ import 'package:my_notes_app/views/notes_view.dart';
 import 'constants.dart';
 import 'models/note_model.dart';
 
-void main() async{
-  await Hive.initFlutter();//This initializes Hive and prepares it for use in your application.
-  await Hive.openBox(kNotesBox);//Open a Hive box by calling await Hive.openBox('box_name'). This creates or opens a Hive box with the specified name
- Hive.registerAdapter(NoteModelAdapter());
+void main() async {
+  await Hive
+      .initFlutter(); //This initializes Hive and prepares it for use in your application.
+  await Hive.openBox(
+      kNotesBox); //Open a Hive box by calling await Hive.openBox('box_name'). This creates or opens a Hive box with the specified name
+  Hive.registerAdapter(NoteModelAdapter());
   runApp(const NotesApp());
 }
 
 class NotesApp extends StatelessWidget {
   const NotesApp({super.key});
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -24,11 +24,9 @@ class NotesApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
         brightness: Brightness.dark,
-         //fontFamily: 'Poppins'
+        //fontFamily: 'Poppins'
       ),
-      home:  NotesView(),
+      home: NotesView(),
     );
   }
 }
-
-
