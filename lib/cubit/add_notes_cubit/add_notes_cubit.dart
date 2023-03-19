101c3b3162bd95f44a12afd104f101d17a9b2e91
marwan ali <marwanalizz42@gmail.com>
2023-03-19 16:40:35 +0200

    add note type adaptor

--- lib/cubit/add_notes_cubit/add_notes_cubit.dart
+++ lib/cubit/add_notes_cubit/add_notes_cubit.dart
@@ -1,5 +1,9 @@
+import '../../models/note_model.dart';
 import 'add_notes_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(AddNoteInitialState());
+
+  addNote(NoteModel note) {}
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,12 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:my_notes_app/views/notes_view.dart';
 
 import 'constants.dart';
+import 'models/note_model.dart';
 
 void main() async{
   await Hive.initFlutter();//This initializes Hive and prepares it for use in your application.
   await Hive.openBox(kNotesBox);//Open a Hive box by calling await Hive.openBox('box_name'). This creates or opens a Hive box with the specified name
+ Hive.registerAdapter(NoteModelAdapter());
   runApp(const NotesApp());
 }
 
